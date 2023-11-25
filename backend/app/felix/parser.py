a632edbb2c7872a3ce9b1195535e429484a74bce
Felix <92216380+Felix200260@users.noreply.github.com>
2023-11-25 17:55:08 +0500

    коммит

--- backend/app/felix/parser.py
+++ backend/app/felix/parser.py
@@ -1,28 +1,30 @@
 from bs4 import BeautifulSoup
 
-def parse_local_html(file_path):
-    try:
-        # Открываем файл для чтения (в режиме текста)
-        with open(file_path, 'r', encoding='utf-8') as file:
+# Здесь предполагается, что html_content - это ваш HTML-код, который вы загрузили
+
+file_path = "index.html"
+
+with open(file_path, 'r', encoding='utf-8') as file:
             # Читаем содержимое файла
             html_content = file.read()
 
-        # Создаем объект BeautifulSoup
-        soup = BeautifulSoup(html_content, 'html.parser')
+soup = BeautifulSoup(html_content, 'html.parser')
+# print(soup)
 
-        # Теперь можно парсить soup так же, как и при запросах через requests
-        # Например, найдем все элементы с определенным классом
-        elements = soup.find_all('div', class_='ygtvitem')
-        for element in elements:
-            print(element.text)
+# Находим корневой div
+root_div = soup.find('div', {'class': 'ygtvchildren', 'id': 'ygtvc1'})
+# print(root_div)
+# Находим все вложенные div с классом ygtvitem
+items = root_div.find_all('div', class_='ygtvitem')
+print(items)
 
-        # Возвращаем результаты
-        return elements
-    except Exception as e:
-        print(f"Error: {e}")
-
-# Путь к вашему HTML файлу
-file_path = 'index.html'
-
-# Вызываем функцию
-parse_local_html(file_path)
+# Извлекаем информацию из каждого элемента
+for item in items:
+    # Находим внутренний div с текстовой информацией
+    text_div = item.find('div', class_='ygtvcontent')
+    if text_div:
+        # Извлекаем текст, удаляя лишние пробелы
+        text = text_div.get_text(strip=True)
+        # Дополнительно обрабатываем текст, если это необходимо
+        text = text.replace('\n', ' ').replace('\r', ' ').strip()
+        print(text)
