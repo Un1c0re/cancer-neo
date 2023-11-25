e3d22e44422abc605e7db56fe2fae5216e97f0b7
Felix <92216380+Felix200260@users.noreply.github.com>
2023-11-25 16:34:06 +0500

    Получение кода 200

--- backend/app/felix/parser.py
+++ backend/app/felix/parser.py
@@ -1,5 +1,25 @@
 import requests
+from bs4 import BeautifulSoup
 
-def parser(url):
-    url = "https://icd.who.int/browse11/l-m/ru"
-    pass
+def parse_icd_site(url):
+    try:
+        # Отправляем запрос на сайт
+        response = requests.get(url)
+        response.raise_for_status()  # Проверяем, что запрос выполнен успешно
+
+        # Создаём объект BeautifulSoup для анализа HTML
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        div_elements = soup.find_all('div', class_='ygtvitem')
+        print(div_elements)
+        
+        # здесь то чтот парсим
+        
+        # return 
+    except Exception as e:
+        return f"Error: {e}"
+
+# Пример использования функции
+url = "https://icd.who.int/browse11/l-m/ru"
+result = parse_icd_site(url)
+print()
