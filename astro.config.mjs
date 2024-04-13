caaadb5b8268ca354649de6c168b094d3e46bc8b
Un1c0re <sayapin.p.a02@gmail.com>
2024-04-13 23:23:18 +0500

    config website

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://un1c0re.github.io',
-  base:'neo-docs',
+  base:'cancer-neo',
   integrations: [
     tailwind(),
   ]
